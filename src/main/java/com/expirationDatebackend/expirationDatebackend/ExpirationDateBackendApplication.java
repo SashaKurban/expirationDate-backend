c0a1e9b8f0d3b9968e94cdced8ed65cd3d0afabd
SashaKurban <sasha_kurbanova@yahoo.com>
2022-08-26 18:32:01 -0400

    springboot initializer setup

---
+++ src/main/java/com/expirationDatebackend/expirationDatebackend/ExpirationDateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.expirationDatebackend.expirationDatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpirationDateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpirationDateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expirationDatebackend/expirationDatebackend/ExpirationDateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expirationDatebackend.expirationDatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpirationDateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
