c070234ad1804add671fbca8815bd075c4504a01
SashaKurban <sasha_kurbanova@yahoo.com>
2022-09-02 10:11:01 -0400

    added daysLeft and expirationDate fields to Grocery

--- src/main/java/com/expirationDatebackend/expirationDatebackend/grocery/Grocery.java
+++ src/main/java/com/expirationDatebackend/expirationDatebackend/grocery/Grocery.java
@@ -2,6 +2,7 @@ package com.expirationDatebackend.expirationDatebackend.grocery;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 @Entity
@@ -23,6 +24,9 @@ public class Grocery {
     private String brand;
     private LocalDate dateOpened;
     private Integer daysToConsume;
+    private LocalDate expirationDate;
+    @Transient
+    private Integer daysLeft;
 
     public Grocery() {
     }
@@ -34,6 +38,7 @@ public class Grocery {
         this.daysToConsume = daysToConsume;
         //if no dateOpened given, use current date
         this.dateOpened = LocalDate.now();
+        this.expirationDate = dateOpened.plusDays(daysToConsume);
     }
 
     public Grocery(String type, String name, String brand, LocalDate dateOpened, Integer daysToConsume) {
@@ -42,6 +47,17 @@ public class Grocery {
         this.brand = brand;
         this.dateOpened = dateOpened;
         this.daysToConsume = daysToConsume;
+        this.expirationDate = dateOpened.plusDays(daysToConsume);
+    }
+
+    public Grocery(Long id, String type, String name, String brand, LocalDate expirationDate) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.brand = brand;
+        this.dateOpened = LocalDate.now();
+        this.expirationDate = expirationDate;
+        this.daysToConsume = Period.between( LocalDate.now(), expirationDate).getDays();
     }
 
     public String getType() {
@@ -68,6 +84,14 @@ public class Grocery {
         return daysToConsume;
     }
 
+    public LocalDate getExpirationDate(){
+        return expirationDate;
+    }
+
+    public Integer getDaysLeft() {
+        return Period.between(LocalDate.now(), expirationDate).getDays();
+    }
+
     public void setType(String type) {
         this.type = type;
     }
@@ -88,6 +112,9 @@ public class Grocery {
         this.daysToConsume = daysToConsume;
     }
 
+    public void setExpirationDate(LocalDate expirationDate){
+        this.expirationDate= expirationDate;
+    }
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yyyy");
@@ -98,6 +125,10 @@ public class Grocery {
                 ", brand='" + brand + '\'' +
                 ", dateOpened=" + dateOpened.format(formatter) +
                 ", daysToConsume=" + daysToConsume +
+                ", expirationDate =" + expirationDate+
+                ", days left= " + this.getDaysLeft() +
                 '}';
     }
+
+
 }
