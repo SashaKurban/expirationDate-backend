393cc82e252cd4ffd9d7ef36703b4d42ca5bac26
SashaKurban <sasha_kurbanova@yahoo.com>
2022-09-15 11:58:14 -0400

    added CORS policies

--- src/main/java/com/expirationDatebackend/expirationDatebackend/grocery/GroceryController.java
+++ src/main/java/com/expirationDatebackend/expirationDatebackend/grocery/GroceryController.java
@@ -16,21 +16,25 @@ public class GroceryController {
         this.groceryService = groceryService;
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "https://expiration-date.vercel.app"})
     @GetMapping
     public List<Grocery> getGroceries(){
         return groceryService.getGroceries();
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "https://expiration-date.vercel.app"})
     @PostMapping
     public void addNewGrocery(@RequestBody Grocery grocery){
         groceryService.addNewGrocery(grocery);
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "https://expiration-date.vercel.app"})
     @DeleteMapping(path = "{groceryId}")
     public void deleteGrocery(@PathVariable("groceryId") Long groceryId){
         groceryService.deleteGrocery(groceryId);
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "https://expiration-date.vercel.app"})
     @PutMapping(path="{groceryId}")
     public void updateGrocery(@PathVariable("groceryId") Long groceryId,
                               @RequestParam(required=false) String name,
